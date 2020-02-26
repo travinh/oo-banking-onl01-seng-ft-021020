4e14bafd8bea0f82b2ea4e4feaccda6d052305c2
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 08:34:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
 end
