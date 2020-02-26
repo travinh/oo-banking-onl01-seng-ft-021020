88c1cc908f0ffdb04736b05f56d8bd93d6414935
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 08:24:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initializealso (name)
+  def initialize (name)
     @name = name 
     @balance = 1000
     @status = "open"
