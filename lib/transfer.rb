88cd129e7a0fbc1703b8e11422e100dedec3f7e5
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 08:48:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,8 @@ class Transfer
       @sender.balance -=amount
       @status = "complete"
     else 
-      "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
       
   end
@@ -33,6 +33,7 @@ class Transfer
     if @status == "complete"
       @receiver.balance -=amount 
       @sender.balance +=amount
+      @status = "reversed"
     end
   end
   
